b21e3cd6a4deef6b298dbf2060e8b97475b3aed4
iquadri3 <iquadri3@gatech.edu>
2020-02-02 20:25:29 -0500

    resolving checkstyle issues
    current checkstyle score: 9/10

--- src/Main.java
+++ src/Main.java
@@ -28,16 +28,89 @@ public class Main extends Application {
     private Scene scene2;
     private Scene scene3;
 
+    private HBox back = new HBox();
+    private int skillPoints = 16;
+    private int startingCredits = 1000;
+    private int t1val = 0;
+    private int t2val = 0;
+    private int t3val = 0;
+    private int t4val = 0;
+
+    /**
+     * This inner class is for the Character
+     */
+    public class Character {
+        private String name;
+        private int skillPoints;
+        private int credits;
+        private int trait1val;
+        private int trait2val;
+        private int trait3val;
+        private int trait4val;
+
+        //GETTER AND SETTER FOR CHARACTER
+        public Character() {
+            this.name = null;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getSkillPoints() {
+            return this.skillPoints;
+        }
+
+        public void setSkillPoints(int skillPoints) {
+            this.skillPoints = skillPoints;
+        }
+
+        public int getCredits() {
+            return this.credits;
+        }
+
+        public void setCredits(int credits) {
+            this.credits = credits;
+        }
+
+        public int getTrait1Val() {
+            return this.trait1val;
+        }
+
+        public void setTrait1Val(int trait1val) {
+            this.trait1val = trait1val;
+        }
+
+        public int getTrait2Val() {
+            return this.trait2val;
+        }
+
+        public void setTrait2Val(int trait2val) {
+            this.trait2val = trait2val;
+        }
+
+        public int getTrait3Val() {
+            return this.trait3val;
+        }
+
+        public void setTrait3Val(int trait3val) {
+            this.trait3val = trait3val;
+        }
+
+        public int getTrait4Val() {
+            return this.trait4val;
+        }
+
+        public void setTrait4Val(int trait4val) {
+            this.trait4val = trait4val;
+        }
+
+    }
     //CHARACTER CONFIGURATION SETUP
-
-    HBox back = new HBox();
-    int skillPoints = 16;
-    int startingCredits = 1000;
-    int t1val = 0;
-    int t2val = 0;
-    int t3val = 0;
-    int t4val = 0;
-
 
     public void format() {
         //Format the backing hbox
@@ -311,81 +384,6 @@ public class Main extends Application {
         });
     }
     //CHARACTER TRAIT ALLOCATOR ENDS
-
-    /**
-     * This inner class is for the Character
-     */
-    public class Character {
-        private String name;
-        private int skillPoints;
-        private int credits;
-        private int trait1val;
-        private int trait2val;
-        private int trait3val;
-        private int trait4val;
-
-        //GETTER AND SETTER FOR CHARACTER
-        public Character() {
-            this.name = null;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getSkillPoints() {
-            return this.skillPoints;
-        }
-
-        public void setSkillPoints(int skillPoints) {
-            this.skillPoints = skillPoints;
-        }
-
-        public int getCredits() {
-            return this.credits;
-        }
-
-        public void setCredits(int credits) {
-            this.credits = credits;
-        }
-
-        public int getTrait1Val() {
-            return this.trait1val;
-        }
-
-        public void setTrait1Val(int trait1val) {
-            this.trait1val = trait1val;
-        }
-
-        public int getTrait2Val() {
-            return this.trait2val;
-        }
-
-        public void setTrait2Val(int trait2val) {
-            this.trait2val = trait2val;
-        }
-
-        public int getTrait3Val() {
-            return this.trait3val;
-        }
-
-        public void setTrait3Val(int trait3val) {
-            this.trait3val = trait3val;
-        }
-
-        public int getTrait4Val() {
-            return this.trait4val;
-        }
-
-        public void setTrait4Val(int trait4val) {
-            this.trait4val = trait4val;
-        }
-
-    }
 
 
     //START METHOD FOR SPACE TRADER APP

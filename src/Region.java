085497b3762fb122cf5e8bb7a1ee1a655a0f6bbc
Hayden Smith <hsmith307@gatech.edu>
2020-02-04 11:47:35 -0500

    adding getters and setters

--- src/Region.java
+++ src/Region.java
@@ -1,4 +1,47 @@
 package src;
 
 public class Region {
+
+	private int xCoord;
+	private int yCoord;
+	private int technologyLevel;
+	private String description;
+
+	public Region() {
+
+	}
+
+	public int getxCoord() {
+		return this.xCoord;
+	}
+
+	public int getyCoord() {
+		return this.yCoord;
+	}
+
+	public int getTechnologyLevel() {
+		return this.technologyLevel;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setxCoord(int num) {
+		this.xCoord = num;
+	}
+
+	public void setyCoord(int num) {
+		this.yCoord = num;
+	}
+
+	public void setTechnologyLevel(int num) {
+		this.technologyLevel = num;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
 }

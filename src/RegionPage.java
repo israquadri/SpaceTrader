b116b78194623b6cfd9958910174a3fc096e0d50
Hayden Smith <hsmith307@gatech.edu>
2020-02-05 19:05:13 -0500

    added random region generator functionality

--- src/RegionPage.java
+++ src/RegionPage.java
@@ -7,28 +7,31 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 
 import java.awt.*;
+import java.util.Random;
 
 public class RegionPage {
 
-	public RegionPage(Stage primaryStage) {
+	public RegionPage(Stage primaryStage, Player p1, Region region) {
 		//make the page in the constructor
 		//follow the same structure, but do it here
 		//set on action methods can go here if there is a button in this page
 		VBox vbox = new VBox();
+
 		Scene regionRoot = new Scene(vbox, 800, 800);
-		primaryStage.setTitle("Into the Universe!");
-		Text text = new Text("Regions of the Universe");
-		text.setStyle("-fx-font-size: 40px");
-		text.setFill(Color.WHITE);
-		BackgroundImage myBI = new BackgroundImage(new Image("galaxy.jpg", 800,
-				800, false, true), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
-				BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+		primaryStage.setTitle(region.getDescription());
+		Text text1 = new Text(region.getDescription());
+		Text text2 = new Text("Coordinates: " + region.getxCoord() + ", " + region.getyCoord());
+		text1.setStyle("-fx-font-size: 40px");
+		text1.setFill(Color.WHITE);
+		text2.setFill(Color.WHITE);
+		BackgroundImage myBI = new BackgroundImage(region.getImg(), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 		vbox.setBackground(new Background(myBI));
 
 
-		vbox.getChildren().addAll(text);
+		vbox.getChildren().addAll(text1, text2);
 		primaryStage.setScene(regionRoot);
 
 	}

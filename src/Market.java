8e41335b3af1bf01ff953e236a995224a1195f00
Sarah Yoo <gayeon1505@gmail.com>
2020-02-18 12:31:37 -0500

    add basic attributes for market

--- src/Market.java
+++ src/Market.java
@@ -1,4 +1,13 @@
 package src;
+import java.util.ArrayList;
 
 public class Market {
+    private String name;
+    private ArrayList<Item> items;
+    private double tax;
+    public Market() {
+        
+    }
+
+
 }

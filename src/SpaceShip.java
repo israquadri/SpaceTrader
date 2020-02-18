1672a9f44a0df7e31367f90cde8226364e04fdf9
Hayden Smith <hsmith307@gatech.edu>
2020-02-18 13:55:17 -0500

    updating spaceship class

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -5,11 +5,18 @@ import java.util.ArrayList;
 public class SpaceShip {
     int fuel;
     ArrayList<Item> inventory;
+    String name;
+    int cargoCapacity;
+    int health;
 
 
-    public SpaceShip(int fuel) {
+    public SpaceShip(int fuel, String name, int cargoCapacity, int health) {
         this.fuel = fuel;
         inventory = new ArrayList<Item>();
+        this.name = name;
+        this.cargoCapacity = cargoCapacity;
+        this.health = health;
+
     }
     public void addToInventory(Item i) {
         inventory.add(i);
@@ -31,4 +38,15 @@ public class SpaceShip {
         this.fuel = fuel;
     }
 
+    public int getHealth() {
+        return health;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

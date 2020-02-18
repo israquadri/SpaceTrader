0761d3944b2d8dcd5f52a24d6e84d6ae621f9b1a
Sjoyner8 <45107110+savannahjoyner@users.noreply.github.com>
2020-02-18 12:30:53 -0500

    item class

--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,42 @@
 package src;
 
 public class Item {
+    
+    private int sellPrice;
+    private int buyPrice;
+    private String name;
+    private String description;
+    private int quantity;
+
+    public Item(int tax, int merchantLevel, int technologyLevel, String name, String description, int quantity) {
+        sellPrice = (tax * merchantLevel);
+        buyPrice = (int)((tax * merchantLevel) * (.75));
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public int getBuyPrice() {
+        return buyPrice;
+    }
+
+    public int getSellPrice() {
+        return sellPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
 
 
 }

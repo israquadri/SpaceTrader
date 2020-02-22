e5fb93a5848f6f1ba1bacff30ba42a23f9c1ce98
Sjoyner8 <45107110+savannahjoyner@users.noreply.github.com>
2020-02-22 11:41:28 -0500

    styled inventory page a bit

--- src/InventoryPage.java
+++ src/InventoryPage.java
@@ -1,17 +1,23 @@
 package src;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+import javafx.util.Duration;
 
 import java.util.Map;
 
@@ -20,6 +26,11 @@ public class InventoryPage {
     public InventoryPage(Stage primaryStage, Player p1, Region region, Region[] array) {
         VBox root = new VBox(40);
         Scene inventory = new Scene(root, 800, 800);
+        inventory.getStylesheets().add("https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap");
+        BackgroundImage myBI = new BackgroundImage(new Image("interior.png", 800,
+                800, false, true), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        root.setStyle("-fx-background-color: rgb(105,105,105);");
 
         //Welcome text for market
         Text welcome = new Text(p1.getName() + "'s Inventory");
@@ -35,11 +46,36 @@ public class InventoryPage {
             SpaceshipInterior r = new SpaceshipInterior(primaryStage, p1, array);
         });
 
-        HBox myItems = new HBox();
-        for (Map.Entry<Item, Integer> i: p1.getSpaceShip().getInventory().entrySet()) {
-            Button item = new Button("" + i.getKey().getName() + ": " + i.getValue());
-            myItems.getChildren().add(item);
-        }
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(25,25,25,25));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setAlignment(Pos.CENTER);
+        grid.getStyleClass().add("grid");
+        //grid.setStyle(" -fx-background-radius: 25;");
+        SpaceShip mySpaceship = p1.getSpaceShip();
+        int cols=2, colCnt = 0, rowCnt = 0;
+        for (Item i: p1.getSpaceShip().getInventory().keySet()) {
+            Button myItem = new Button("" + i.getName());
+            myItem.setGraphic(new ImageView(i.getImage()));
+            myItem.setContentDisplay(ContentDisplay.TOP);
+            Tooltip preSale = new Tooltip("Sell Price: " + i.getSellPrice() + "\n"
+                    + " Quantity:" + mySpaceship.getQuantity(i));
+            preSale.setShowDelay(Duration.ZERO);
+            myItem.setTooltip(preSale);
+            myItem.setStyle("-fx-font-family: 'Press Start 2P', cursive; -fx-text-fill: white;"
+                    + "-fx-background-color: rgb(128,128,128); -fx-font-size: 10px;");
+
+                grid.add(myItem,colCnt,rowCnt);
+                colCnt++;
+
+                if (colCnt>cols) {
+                    rowCnt++;
+                    colCnt=0;
+                }
+            }
+
 
         //Drop shadow effect
         DropShadow shadow = new DropShadow();
@@ -64,7 +100,7 @@ public class InventoryPage {
                 });
 
         //Adding different hboxes to root vbox node
-        root.getChildren().addAll(back, welcome, myItems);
+        root.getChildren().addAll(back, welcome, grid);
 
         //Making scene show
         primaryStage.setScene(inventory);

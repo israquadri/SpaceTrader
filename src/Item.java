06cd86fd304315a3999bef4c6552a0557b739918
Sarah Yoo <gayeon1505@gmail.com>
2020-03-07 01:38:57 -0500

    add new Item constructor for trader + add setBuyPrice()

--- src/Item.java
+++ src/Item.java
@@ -29,9 +29,23 @@ public class Item {
         this.quantity = quantity;
         this.isStolen = false;
     }
+    //constructor for items without image
+    public Item(double tax, int merchantLevel, int technologyLevel, String name,
+                int quantity, int basePrice, boolean trader) {
+        sellPrice = (int) (basePrice * (1 + tax + .01 * technologyLevel));
+        sellPrice = (int) (varRand.nextInt(sellPrice) + .5 * sellPrice);
+        sellPrice = (int) (sellPrice - (.02 * merchantLevel * sellPrice));
+        buyPrice = (int) ((sellPrice) * (.75));
+        this.name = name;
+        this.quantity = quantity;
+    }
 
     public int getBuyPrice() {
         return buyPrice;
+    }
+
+    public void setBuyPrice(int newPrice) {
+        buyPrice = newPrice;
     }
 
     public int getSellPrice() {

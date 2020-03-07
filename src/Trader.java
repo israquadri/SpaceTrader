afe39760e6613ea5fabd517a498c55651c381974
Sarah Yoo <gayeon1505@gmail.com>
2020-03-07 01:19:44 -0500

    update trader and traderEncounterPage

--- src/Trader.java
+++ src/Trader.java
@@ -1,5 +1,56 @@
 package src;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Trader {
-    
+    private ArrayList<Item> inventory;
+    private Player player;
+    private Item itemToSell;
+
+    public Trader(String[] itemNames, Player p1) {
+        this.player = p1;
+        int randomTechLevel = new Random().nextInt(8) + 1;
+        int merchantSkill = player.getMerchantSkill();
+        double tax = new Random().nextDouble();
+        ItemListInitializer itemInit = new ItemListInitializer();
+        inventory = itemInit.createTraderItemList(randomTechLevel, merchantSkill, tax, itemNames);
+        itemToSell = inventory.get(new Random().nextInt(inventory.size()));
+    }
+
+    public boolean determineSuccess(int skillLevel) {
+        if (skillLevel == 0) {return false;}
+        return (new Random().nextInt(8) + 1 <=  skillLevel);
+    }
+
+    //halves the buyPrice of itemToSell
+    public void decreasePrice() {
+        //price halved
+        //is it okay if the price becomes 0?
+        itemToSell.setBuyPrice(itemToSell.getBuyPrice() / 2);
+    }
+
+    //doubles the buyPrice of itemToSell
+    public void increasePrice() {
+        //price doubled
+        itemToSell.setBuyPrice(itemToSell.getBuyPrice() * 2);
+    }
+
+    /**
+     * Gets inventory.
+     *
+     * @return Value of inventory.
+     */
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    /**
+     * Gets itemToSell.
+     *
+     * @return Value of itemToSell.
+     */
+    public Item getItemToSell() {
+        return itemToSell;
+    }
 }
--- src/TraderEncounterPage.java
+++ src/TraderEncounterPage.java
@@ -1,4 +1,30 @@
 package src;
 
+import javafx.stage.Stage;
+
 public class TraderEncounterPage {
+    /*
+    * Option 1 - Buy Item:
+    *   - itemSold = trader.getItemToSell()
+    *   * check if the player has enough credit to purchase item(s)
+    *   - push (itemSold, itemSold.getQuantity()) to player's inventory
+    *   * I don't think the trader's inventory needs to be updated since it doesn't affect anything
+    *
+    * Option 2 - Ignore Trader:
+    *   - continue to intended destination
+    *
+    * Option 3 - Rob trader:
+    *   1. Determine the player's success by doing trader.determineSuccess(player.getFighterSkill)
+    *       - if (success) -> player robs itemToSell
+    *         * the rubric only says "some of trader's items are added to player inventory" so I guess it doesn't really
+    *            matter which item/how many of them is stolen?
+    *       - else -> player's ship health lowered, continues to intended destination
+    * Option 4 - Negotiate price
+    *   1. trader.determineSuccess(player.getMerchantSkill)
+    *       - if (success) -> trader.decreasePrice()
+    *       - else -> trader.increasePrice()
+    *  2. get rid of option 4 since the player can negotiate for price only once
+    *  */
+    public TraderEncounterPage(Stage primaryStage, Region[] regions, Player p1, Trader trader) {}
+
 }

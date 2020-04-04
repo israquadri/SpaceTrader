c397826113bd8ebc7a37fc256c4d8e0047e38009
Hayden Smith <hsmith307@gatech.edu>
2020-04-03 21:15:43 -0400

    added a page so you can start over

--- src/WinGamePage.java
+++ src/WinGamePage.java
@@ -1,8 +1,28 @@
 package src;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+
+import java.awt.*;
 
 public class WinGamePage {
 
@@ -10,8 +30,14 @@ public class WinGamePage {
 		VBox root = new VBox();
 		Scene scene = new Scene(root, 800, 800);
 
+		Button playAgain = new Button("Start Game Over");
+		playAgain.setOnMouseClicked((MouseEvent m) -> {
+			WelcomePage welcomePage = new WelcomePage(primaryStage);
+		});
 
-		primaryStage.setTitle("you win");
+
+		root.getChildren().addAll(playAgain);
+		primaryStage.setTitle("You Win the Game!");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
